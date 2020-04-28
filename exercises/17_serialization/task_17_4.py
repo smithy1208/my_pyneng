df0df2ff29fd245275631923226f876e8a69c8f6
Vladimir Kuznetsov <smithy@smithy-gg.avalanchelab.com>
2020-04-28 13:21:29 +0300

    tasks 17.4 done

--- exercises/17_serialization/task_17_4.py
+++ exercises/17_serialization/task_17_4.py
@@ -37,6 +37,8 @@ C-3PO,c3po@gmail.com,16/12/2019 17:24
 """
 
 import datetime
+import csv
+from pprint import pprint
 
 
 def convert_datetimestr_to_datetime(datetime_str):
@@ -44,3 +46,40 @@ def convert_datetimestr_to_datetime(datetime_str):
     Конвертирует строку с датой в формате 11/10/2019 14:05 в объект datetime.
     """
     return datetime.datetime.strptime(datetime_str, "%d/%m/%Y %H:%M")
+
+
+def write_last_log_to_csv(source_log, output):
+    '''
+    Аргументы функции:
+    * source_log - имя файла в формате csv, из которого читаются данные (пример mail_log.csv)
+    * output - имя файла в формате csv, в который будет записан результат
+
+    Функция ничего не возвращает.
+    '''
+    with open(source_log) as src:
+        reader = csv.reader(src)
+        headers = next(reader)
+        # print('Headers: ', headers)
+
+        email_dict = {}
+        for row in reader:
+            name, email, last_changed = row
+
+            if not email in email_dict.keys():
+                email_dict[email] = [name, last_changed]
+            else:
+                old_data = email_dict[email][1]
+                if convert_datetimestr_to_datetime(old_data) < convert_datetimestr_to_datetime(last_changed):
+                    email_dict[email] = [name, last_changed]
+
+    # pprint(email_dict)
+    with open(output, 'w') as dst:
+        writer = csv.writer(dst)
+        writer.writerow(headers)
+        for email, item in email_dict.items():
+            name, last_changed = item
+            writer.writerow([name, email, last_changed])
+
+
+if __name__ == '__main__':
+    write_last_log_to_csv('mail_log.csv', 'mail_out.csv')
